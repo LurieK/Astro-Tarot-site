252253ba11513a1dbd523d4992208e7a684ae41c
LurieK <Lurainekimmerle@gmail.com>
2023-01-22 16:40:30 +0800

    design drop down, and start JS

--- test-ideas.js
+++ test-ideas.js
@@ -5,4 +5,10 @@ const threeCardFuture = document.getElementById ('card3');
 
 //add the selected tarot card to card1
 
+const spread= document.querySelector('.spread'),
+selectBtn= spread.querySelector('.select-btn'),
 
+selectBtn.addEventListener('click',()=> {
+
+  spread.classList.toggle('active';)
+})
