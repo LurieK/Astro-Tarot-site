88c1f77ae1d2f3c17925b6d4b9a2c9f17a3ba4a5
LurieK <Lurainekimmerle@gmail.com>
2023-01-22 13:01:15 +0800

    added card list

--- test-ideas.js
+++ test-ideas.js
@@ -1,27 +1,8 @@
-const dropdownBtn = document.getElementById("btn");
-const dropdownMenu = document.getElementById("dropdown");
-const toggleArrow = document.getElementById("arrow");
+
 const threeCardPast = document.getElementById ('card1');
 const threeCardPresent = document.getElementById ('card2');
 const threeCardFuture = document.getElementById ('card3');
 
 //add the selected tarot card to card1
 
-// Toggle dropdown function
-const toggleDropdown = function () {
-  dropdownMenu.classList.toggle("show");
-  toggleArrow.classList.toggle("arrow");
-};
 
-// Toggle dropdown open/close when dropdown button is clicked
-dropdownBtn.addEventListener("click", function (e) {
-  e.stopPropagation();
-  toggleDropdown();
-});
-
-// Close dropdown when dom element is clicked
-document.documentElement.addEventListener("click", function () {
-  if (dropdownMenu.classList.contains("show")) {
-    toggleDropdown();
-  }
-});
