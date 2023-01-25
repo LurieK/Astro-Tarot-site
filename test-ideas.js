3b46e170f0b2bf6af92b0c2ba35b5302827a8fef
LurieK <Lurainekimmerle@gmail.com>
2023-01-25 08:59:04 +0800

    reoganize CSS

--- test-ideas.js
+++ test-ideas.js
@@ -1,25 +1,4 @@
 
-//selects the box user wants to place card in
-
-// const threeCardPast = document.getElementById ('card1');
-// const threeCardPresent = document.getElementById ('card2');
-// const threeCardFuture = document.getElementById ('card3');
-
-
-
-// function highlightBox() {
-//   x.style.borderColor ='green';
-// }
-
-// threeCardFuture.addEventListener('click',highlightBox);
-
-// document.addEventListener('click', function closeCardList(event) {
-  
-//   if (!cardList.contains(event.target)) {
-//     cardList.style.display = 'none';
-//   }
-// })
-
 //opens and closes CardList and Search 
 
 const spread= document.querySelector(".spread")
@@ -37,5 +16,50 @@ document.addEventListener('click', function closeCardList(event) {
   
   if (!spread.contains(event.target)) {
     cardList.forEach(card => {card.style.display = 'none';});
+  }
+});
+
+
+
+//selects the box user wants to place card in
+
+const threeCardPast = document.getElementById ('card1');
+const threeCardPresent = document.getElementById ('card2');
+const threeCardFuture = document.getElementById ('card3');
+const allThree = document.getElementsByClassName('.three-cards');
+const spreadSpace= document.getElementsByClassName('.spread-space')
+
+
+function highlightFirstBox() {
+  threeCardPast.style.borderColor ='red';
+}
+threeCardPast.addEventListener('click', highlightFirstBox);
+
+
+
+function highlightSecondBox() {
+  threeCardPresent.style.borderColor ='red';
+}
+
+threeCardPresent.addEventListener('click', highlightSecondBox);
+
+function highlightThirdBox() {
+  threeCardFuture.style.borderColor ='red';
+}
+
+threeCardFuture.addEventListener('click', highlightThirdBox);
+
+//unselect box when clicking outside of the square
+
+// function resetCards(){
+//   threeCardPast.style.borderColor = '';
+//   threeCardPresent.style.borderColor = '';
+//   threeCardFuture.style.borderColor = '';
+// }
+
+document.addEventListener('click', function unhighlightCard(event) {
+  
+  if (!spreadSpace.contains(event.target)) {
+    allThree.forEach(box => {box.style.borderColor = '';});
   }
 });
